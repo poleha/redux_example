8e9781b2234625782afe2f49373fec86c63b237d
poleha <pass1191>
2016-04-13 09:18:03 +0300

    Changes

--- src/store/enhancers/ping-compiled.js
+++ src/store/enhancers/ping-compiled.js
@@ -4,6 +4,12 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 /*eslint-disable */
+//export const ping = store => next => action => {
+//    console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+//    return next(action)
+//}
+/*eslint-enable */
+
 var ping = exports.ping = function ping(store) {
     return function (next) {
         return function (action) {
@@ -12,15 +18,5 @@ var ping = exports.ping = function ping(store) {
         };
     };
 };
-/*eslint-enable */
-
-//var ping = function ping(store) {
-//   return function (next) {
-//        return function (action) {
-//            console.log('ping');
-//            return next(action);
-//        };
-//    };
-//};
 
 //# sourceMappingURL=ping-compiled.js.map
--- src/store/enhancers/ping.js
+++ src/store/enhancers/ping.js
@@ -1,17 +1,16 @@
 /*eslint-disable */
-export const ping = store => next => action => {
-    console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
-    return next(action)
-}
+//export const ping = store => next => action => {
+//    console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+//    return next(action)
+//}
 /*eslint-enable */
 
 
-
-//var ping = function ping(store) {
-//   return function (next) {
-//        return function (action) {
-//            console.log('ping');
-//            return next(action);
-//        };
-//    };
-//};
+export const ping = function (store) {
+   return function (next) {
+        return function (action) {
+            console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+            return next(action);
+        };
+    };
+};
