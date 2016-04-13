6e8d651b7356d014c3d12fca63c7bcff55330e2b
poleha <pass1191>
2016-04-13 12:31:51 +0300

    Ping middleware altered

--- src/store/enhancers/ping.js
+++ src/store/enhancers/ping.js
@@ -1,16 +1,16 @@
 /*eslint-disable */
-//export const ping = store => next => action => {
-//    console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
-//    return next(action)
-//}
+export const ping = store => next => action => {
+    console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+    return next(action)
+}
 /*eslint-enable */
 
 
-export const ping = function (store) {
-   return function (next) {
-        return function (action) {
-            console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
-            return next(action);
-        };
-    };
-};
+//export const ping = function (store) {
+//   return function (next) {
+//        return function (action) {
+//            console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+//            return next(action);
+//        };
+//    };
+//};
